1069b985152e55158afed6e1387b68d22e8b961a
Nam Long <tnamlong2132000@gmail.com>
2023-07-23 21:39:42 +0700

    deployed website commit

--- src/component/MoveBrand/CustomerBrandSlider.js
+++ src/component/MoveBrand/CustomerBrandSlider.js
@@ -1,40 +1,28 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import {CustomBrandData} from './CustomBrandData'
 
-export const SimpleSlider = () => {
+export const CustomerBrandSlider = () => {
     const settings = {
         dots: false,
         infinite: true,
-        autoplay:true,
+        autoplay: true,
         autoplaySpeed: 50,
         slidesToShow: 6,
-        slidesToScroll: 1
+        slidesToScroll: 1,
     };
     return (
         <div>
             <Slider {...settings}>
-                <div>
-                    <h3>1</h3>
-                </div>
-                <div>
-                    <h3>2</h3>
-                </div>
-                <div>
-                    <h3>3</h3>
-                </div>
-                <div>
-                    <h3>4</h3>
-                </div>
-                <div>
-                    <h3>5</h3>
-                </div>
-                <div>
-                    <h3>6</h3>
-                </div>
-                <div>
-                    <h3>7</h3>
-                </div>
+                {
+                    CustomBrandData.map((customItem) => (
+                        <div className="cus-content">
+                            <img key={customItem.id} src={customItem.img} alt='/'/>
+                        </div>
+
+                    ))
+                }
             </Slider>
         </div>
     );
